540f639094f5ba38c3a662fcf72d7f1ab386b2b2
Henrik <82148770+Knuddel04@users.noreply.github.com>
2021-04-08 21:28:17 +0200

    Update Voice-erkennung.py

--- Voice-erkennung.py
+++ Voice-erkennung.py
@@ -21,9 +21,9 @@ def music():
     if text == "play music":
         musi = "your music is playing"
         tts = gTTS(musi)
-        if os.path.exists(r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3"):
-            os.remove(r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3")
-        filename = r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3"
+        if os.path.exists(r"path"):
+            os.remove(r"path")
+        filename = r"path"
         tts = tts.save(filename)
         playsound.playsound(filename)
         
@@ -33,9 +33,9 @@ def light():
         webbrowser.open("https://www.bleiben-sie-sicher.de/wp-content/uploads/2016/12/EswerdeLicht-800x480.png")
         lights = "lights are on now"
         tts = gTTS(lights)
-        if os.path.exists(r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3"):
-            os.remove(r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3")
-        filename = r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3"
+        if os.path.exists(r"path"):
+            os.remove(r"path")
+        filename = r"path"
         tts = tts.save(filename)
         playsound.playsound(filename)
 
@@ -44,9 +44,9 @@ def yt():
         webbrowser.open("https://www.youtube.com/?gl=DE")
         yot = "opened youtube"
         tts = gTTS(yot)
-        if os.path.exists(r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3"):
-            os.remove(r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3")
-        filename = r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3"
+        if os.path.exists(r"path"):
+            os.remove(r"path")
+        filename = r"path"
         tts = tts.save(filename)
         playsound.playsound(filename)
 
@@ -55,9 +55,9 @@ def google():
         webbrowser.open("https://www.google.com")
         gog = "opened google"
         tts = gTTS(gog)
-        if os.path.exists(r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3"):
-            os.remove(r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3")
-        filename = r"C:\Users\hg04\Documents\Programmieren\AA_Python\Projekte\Voice-erkennung\sprache.mp3"
+        if os.path.exists(r"path"):
+            os.remove(r"path")
+        filename = r"path"
         tts = tts.save(filename)
         playsound.playsound(filename)
 
